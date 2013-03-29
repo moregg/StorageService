871aa95ffd8dbda97dec8b095e20128c54b0a4be
ycl <chunlong.yu@moregg.com>
2013-03-29 13:43:15 +0800

    add photo.save

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,6 +4,7 @@ class PhotosController < ApplicationController
       photo = params[:photo]
       filter_info = params[:filter_info]
       p = Photo.new(:description => params[:description])
+      p.save
 
       File.open("public/" + p.id.to_s + ".jpg", "wb+") do |f|
         f.write(params[:photo].read)
