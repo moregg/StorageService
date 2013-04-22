7d3d58312caeac4fb0a970748d1e86026a97d59e
ycl <chunlong.yu@moregg.com>
2013-04-22 14:20:11 +0800

    polish log

--- app/models/mogile_fs_error_log.rb
+++ app/models/mogile_fs_error_log.rb
@@ -1,6 +1,6 @@
 class MogileFsErrorLog
   @@logger = Log4r::Logger.new "mogile_fs_error_log"
-  @@out_putter = Log4r::RollingFileOutputter.new('mogile_fs_error_log', :filename =>  Rails.root.to_s + "log/mogile_fs_error.log", :maxtime=> 24*3600, :trunc => true)
+  @@out_putter = Log4r::RollingFileOutputter.new('mogile_fs_error_log', :filename =>  Rails.root.to_s + "/log/mogile_fs_error.log", :maxtime=> 24*3600, :trunc => true)
   @@pattern_formatter = Log4r::PatternFormatter.new(:pattern => "%d %m\n")
 
   @@out_putter.formatter = @@pattern_formatter
--- app/models/processer_log.rb
+++ app/models/processer_log.rb
@@ -1,6 +1,6 @@
 class ProcesserLog
   @@logger = Log4r::Logger.new "processer_log"
-  @@out_putter = Log4r::RollingFileOutputter.new('processer_log', :filename =>  Rails.root.to_s + "log/processer.log", :maxtime=> 24*3600, :trunc => true)
+  @@out_putter = Log4r::RollingFileOutputter.new('processer_log', :filename =>  Rails.root.to_s + "/log/processer.log", :maxtime=> 24*3600, :trunc => true)
   @@pattern_formatter = Log4r::PatternFormatter.new(:pattern => "%d %m\n")
 
   @@out_putter.formatter = @@pattern_formatter
