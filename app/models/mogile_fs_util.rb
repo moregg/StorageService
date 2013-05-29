61a33a0fe3e284e45fbfdca6fab4411fd58a8322
ycl <chunlong.yu@moregg.com>
2013-05-29 16:21:55 +0800

    refactor

--- app/models/mogile_fs_util.rb
+++ app/models/mogile_fs_util.rb
@@ -1,8 +1,9 @@
 class MogileFsUtil
-  def MogileFsUtil.put_file_to_fs(src_file_path, mg_key, mg_class, mg_domain = MOGILEFS_DOMAIN)
+  @@filestore = MogileFS::MogileFS.new(:domain => MOGILEFS_DOMAIN, :hosts => MOGILEFS_HOSTS)
+
+  def MogileFsUtil.put_file_to_fs(src_file_path, mg_key, mg_class)
       begin
-        filestore = MogileFS::MogileFS.new(:domain => mg_domain, :hosts => MOGILEFS_HOSTS)
-        filestore.store_file(mg_key, mg_class, src_file_path)
+        @@filestore.store_file(mg_key, mg_class, src_file_path)
       rescue Exception=>e
         MogileFsErrorLog.log e.message
         MogileFsErrorLog.log "the file is :" + src_file_path
@@ -12,10 +13,9 @@ class MogileFsUtil
   end
 
 
-  def MogileFsUtil.put_content_to_fs(content, mg_key, mg_class, mg_domain = MOGILEFS_DOMAIN)
+  def MogileFsUtil.put_content_to_fs(content, mg_key, mg_class)
       begin
-        filestore = MogileFS::MogileFS.new(:domain => mg_domain, :hosts => MOGILEFS_HOSTS)
-        filestore.store_content(mg_key, mg_class, content)
+        @@filestore.store_content(mg_key, mg_class, content)
       rescue Exception=>e
         MogileFsErrorLog.log e.message
         MogileFsErrorLog.log "the file is :" + src_file_path
@@ -26,12 +26,11 @@ class MogileFsUtil
 
   def MogileFsUtil.get_data_from_fs(mg_key, mg_class, mg_domain = MOGILEFS_DOMAIN)
     begin
-     # path = TEMP_PATH + '/' + UUIDTools::UUID.timestamp_create().to_s
-      filestore = MogileFS::MogileFS.new(:domain => mg_domain, :hosts => MOGILEFS_HOSTS)
-      data = filestore.get_file_data(mg_key)
+      data = @@filestore.get_file_data(mg_key)
       return data
     rescue Exception=>e
-      puts e.message 
+      puts e.message
+      return nil
     end
   end
 end
